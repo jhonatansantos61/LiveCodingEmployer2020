ac8b3b993bada7bf56417e96af551c0b44de42e1
Maestro <paulomaestro@employer.com.br>
2020-09-04 22:22:58 -0300

    Modified project - Created Model

---
+++ Live.Caqui.Model/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Live.Caqui.Model
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Live.Caqui.Model/VotingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Live.Caqui.Model
+{
+    public class VotingModel
+    {
+        public string Description { get; set; }
+        public int Count { get; set; }
+    }
+}
