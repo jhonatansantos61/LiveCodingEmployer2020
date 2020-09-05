77532c389902016802d12cb45cf88e0e11ec6718
Maestro <paulomaestro@employer.com.br>
2020-09-05 01:25:41 -0300

    Modified project

--- Live.Caqui.Consumption/Login.cs
+++ Live.Caqui.Consumption/Login.cs
@@ -9,10 +9,6 @@ namespace Live.Caqui.Consumption
 {
     public class Login : ILogin
     {
-        private const string Uri = "https://live.paulomaestro.com.br";
-        private const string UriGetUser = Uri + "/Satisfation/GetUser";
-        private const string UriPostUser = Uri + "/Satisfation/PostUser";
-
         private readonly ISyncAsync _iSyncAsync;
         public Login(ISyncAsync ISyncAsync)
         {
@@ -22,7 +18,7 @@ namespace Live.Caqui.Consumption
         {
             var result = "";
             _iSyncAsync.HTTPVerb = HTTPVerb.GET;
-            _iSyncAsync.Url = UriGetUser + "?User=" + User.Login + "&Password=" + User.Password;
+            _iSyncAsync.Url = "https://live.paulomaestro.com.br/Satisfation/GetUser" + "?User=" + User.Login + "&Password=" + User.Password;
             var resultAux = await _iSyncAsync.GoSyncAsync();
 
             if (!resultAux.Item1)
@@ -36,7 +32,7 @@ namespace Live.Caqui.Consumption
         {
             var result = false;
             _iSyncAsync.HTTPVerb = HTTPVerb.POST;
-            _iSyncAsync.Url = UriPostUser;
+            _iSyncAsync.Url = "https://live.paulomaestro.com.br/Satisfation/PostUser";
             _iSyncAsync.Obj = User;
             var resultAux = await _iSyncAsync.GoSyncAsync();
 
